99d1c973eee6c9c38781ac1850d91cf64cead5df
Priya <142904884+ShivaniNagda@users.noreply.github.com>
2025-04-01 11:15:20 +0530

    LogInPage.jsx

--- frontendClient/src/Pages/LogInPage.jsx
+++ frontendClient/src/Pages/LogInPage.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { motion } from "framer-motion";
 import { Mail, User,Lock, Loader } from "lucide-react";
-import Input from "../components/input";
+import Input from "../components/Input";
 import { Link } from "react-router-dom";
 import { userAuthStore } from "../store/authStore";
 
